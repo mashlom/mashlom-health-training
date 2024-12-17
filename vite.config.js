4bfa5391c51ab5d5d6d4369ab9e7cedf69a9f0f3
Yonatan Katz <katzyonatan@gmail.com>
2024-12-17 21:53:05 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/mashlom-health-training/",
+  base: '/',
   plugins: [react()],
 });
