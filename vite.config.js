d68d91e21ae7caa3bda518b1f944e67c307a657b
boaz <gutmanboaz@gmail.com>
2025-01-08 17:09:32 +0200

    no er_2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Check if we're running in production
-const isProduction = process.env.NODE_ENV === "production";
-
 export default defineConfig({
-  base: isProduction ? "/er/" : "/",
+  base: "/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
 });
