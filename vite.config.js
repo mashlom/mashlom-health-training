ef4e5a5dc7e9e86917acc19f75a3ff98a9fc75c7
boaz <gutmanboaz@gmail.com>
2025-01-08 16:49:39 +0200

    adding /er to the url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Check if we're in production mode
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
-  base: "/",
+  base: isProduction ? "/er/" : "/",
   plugins: [react()],
 });
