7cf80eb28efe28317af42197ccf2ef5362cd0fc6
relbns <ariel@stoketalent.com>
2025-02-21 06:50:19 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const base = process.env.VITE_BASE_PATH || '/';
-
 export default defineConfig({
-  base,
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
 });
