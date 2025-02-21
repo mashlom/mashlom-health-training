45b50decdc5075376a2b8409cbf572b565d9d3a1
relbns <ariel@stoketalent.com>
2025-02-21 13:55:59 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
   },
 });
