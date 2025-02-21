cccecd9488abb729d0ca55bddfc769b34b07e45f
relbns <ariel@stoketalent.com>
2025-02-21 14:16:42 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      },
-    },
   },
 });
